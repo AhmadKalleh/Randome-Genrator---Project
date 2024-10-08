cddaa8e5895c32ef3d2125bb4ad0cb9c78f6f437
AhmadKalleh <ahmadhkalleh@gmail.com>
2024-10-08 11:35:14 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Randome_Genrator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.rbSpecialCharacter = new System.Windows.Forms.RadioButton();
+            this.rbNumbers = new System.Windows.Forms.RadioButton();
+            this.rbLetter = new System.Windows.Forms.RadioButton();
+            this.LetterSizeStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CapitalLetter = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.SmallLetter = new System.Windows.Forms.ToolStripMenuItem();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.LetterSizeStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbResult
+            // 
+            this.lbResult.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.ForeColor = System.Drawing.Color.Cornsilk;
+            this.lbResult.Location = new System.Drawing.Point(219, 154);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(400, 27);
+            this.lbResult.TabIndex = 1;
+            this.lbResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.rbSpecialCharacter);
+            this.groupBox1.Controls.Add(this.rbNumbers);
+            this.groupBox1.Controls.Add(this.rbLetter);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(178, 260);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(467, 222);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Details";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.Color.BurlyWood;
+            this.radioButton1.Location = new System.Drawing.Point(307, 155);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 31);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.Text = "Mix";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbSpecialCharacter
+            // 
+            this.rbSpecialCharacter.AutoSize = true;
+            this.rbSpecialCharacter.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSpecialCharacter.ForeColor = System.Drawing.Color.BurlyWood;
+            this.rbSpecialCharacter.Location = new System.Drawing.Point(29, 155);
+            this.rbSpecialCharacter.Name = "rbSpecialCharacter";
+            this.rbSpecialCharacter.Size = new System.Drawing.Size(237, 31);
+            this.rbSpecialCharacter.TabIndex = 4;
+            this.rbSpecialCharacter.Text = "Special Character";
+            this.rbSpecialCharacter.UseVisualStyleBackColor = true;
+            this.rbSpecialCharacter.CheckedChanged += new System.EventHandler(this.rbSpecialCharacter_CheckedChanged);
+            // 
+            // rbNumbers
+            // 
+            this.rbNumbers.AutoSize = true;
+            this.rbNumbers.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbNumbers.ForeColor = System.Drawing.Color.BurlyWood;
+            this.rbNumbers.Location = new System.Drawing.Point(307, 101);
+            this.rbNumbers.Name = "rbNumbers";
+            this.rbNumbers.Size = new System.Drawing.Size(134, 31);
+            this.rbNumbers.TabIndex = 3;
+            this.rbNumbers.Text = "Numbers";
+            this.rbNumbers.UseVisualStyleBackColor = true;
+            this.rbNumbers.CheckedChanged += new System.EventHandler(this.rbNumbers_CheckedChanged);
+            // 
+            // rbLetter
+            // 
+            this.rbLetter.AutoSize = true;
+            this.rbLetter.Checked = true;
+            this.rbLetter.ContextMenuStrip = this.LetterSizeStrip;
+            this.rbLetter.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbLetter.ForeColor = System.Drawing.Color.BurlyWood;
+            this.rbLetter.Location = new System.Drawing.Point(29, 101);
+            this.rbLetter.Name = "rbLetter";
+            this.rbLetter.Size = new System.Drawing.Size(113, 31);
+            this.rbLetter.TabIndex = 2;
+            this.rbLetter.TabStop = true;
+            this.rbLetter.Text = "Letters";
+            this.rbLetter.UseVisualStyleBackColor = true;
+            this.rbLetter.CheckedChanged += new System.EventHandler(this.rbLetter_CheckedChanged);
+            // 
+            // LetterSizeStrip
+            // 
+            this.LetterSizeStrip.BackColor = System.Drawing.Color.FloralWhite;
+            this.LetterSizeStrip.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LetterSizeStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.LetterSizeStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CapitalLetter,
+            this.toolStripSeparator1,
+            this.SmallLetter});
+            this.LetterSizeStrip.Name = "LetterSizeStrip";
+            this.LetterSizeStrip.Size = new System.Drawing.Size(214, 66);
+            // 
+            // CapitalLetter
+            // 
+            this.CapitalLetter.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CapitalLetter.Name = "CapitalLetter";
+            this.CapitalLetter.Size = new System.Drawing.Size(213, 28);
+            this.CapitalLetter.Text = "Capital Letter";
+            this.CapitalLetter.Click += new System.EventHandler(this.CapitalLetter_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(210, 6);
+            // 
+            // SmallLetter
+            // 
+            this.SmallLetter.Name = "SmallLetter";
+            this.SmallLetter.Size = new System.Drawing.Size(213, 28);
+            this.SmallLetter.Text = "Small Letter";
+            this.SmallLetter.Click += new System.EventHandler(this.SmallLetter_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Teal;
+            this.numericUpDown1.Location = new System.Drawing.Point(246, 36);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(97, 31);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.BurlyWood;
+            this.label2.Location = new System.Drawing.Point(24, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Number Of Digits";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orange;
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(196, 515);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.OrangeRed;
+            this.button3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(483, 515);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label1.Location = new System.Drawing.Point(343, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 43);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Result";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.BackgroundImage = global::Randome_Genrator.Properties.Resources.close;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(736, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 46);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(800, 591);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.LetterSizeStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbSpecialCharacter;
+        private System.Windows.Forms.RadioButton rbNumbers;
+        private System.Windows.Forms.RadioButton rbLetter;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip LetterSizeStrip;
+        private System.Windows.Forms.ToolStripMenuItem CapitalLetter;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem SmallLetter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Randome_Genrator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void SetRoundedCornersToForm()
+        {
+            int radius = 30; // نصف قطر الزوايا الدائرية
+            GraphicsPath path = new GraphicsPath();
+
+            // إضافة مستطيل بزوايا دائرية إلى المسار
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(this.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(this.Width - radius, this.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, this.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            this.Region = new Region(path); // تعيين المنطقة المحددة للمسار
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            SetRoundedCornersToForm(); // إعادة تعيين الزوايا عند تغيير حجم النموذج
+
+        }
+        enum enSize
+        {
+            CapitalLetter = 1,SmallLetter =2
+        }
+
+        
+        enum enStatus
+        {
+            Letters = 1,Numbers = 2,SpecialCharacter = 3,Mix = 4
+        };
+
+
+        enSize size;
+        enStatus status;
+        Random random = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        
+        private String Generate()
+        {
+            String ans = "";         
+            int Number = 0;
+
+
+            switch (status)
+            {
+                case (enStatus.Letters):
+                
+                    switch(size)
+                    {
+                        case enSize.CapitalLetter:
+                        Number = random.Next(65, 91);
+                        ans = Convert.ToString((char)Number);
+                        return ans;
+                      
+
+                        case enSize.SmallLetter:
+                        Number = random.Next(97, 123);
+                        ans = Convert.ToString((char)Number);                          
+                        return ans;
+                            
+                    }
+
+                break;
+
+
+                case enStatus.SpecialCharacter:
+                    Number = random.Next(33, 48);
+                    ans = Convert.ToString((char)Number);
+                    return ans;
+
+                
+
+                case enStatus.Numbers:
+                    Number = random.Next(0, 10);
+                    ans = Convert.ToString(Number);
+                    return ans;
+               
+
+                
+            }
+
+            return "";
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color color = Color.White;
+            Pen pen = new Pen(color);
+
+            pen.Width = 1;
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawLine(pen, 170, 150, 530,150);
+        }
+
+      
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lbResult.Text = "";
+            rbLetter.Checked = true;
+            numericUpDown1.Value = numericUpDown1.Minimum;
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+
+            String Result = "";
+
+            if(status==enStatus.Mix)
+            {
+                for (int i = 1; i <= numericUpDown1.Value; i++)
+                {
+                    int Number = random.Next(1, 4);
+                    
+                    status = (enStatus)Number;
+
+                    if(status==enStatus.Letters)
+                    {
+                        int Number2 = random.Next(1, 3);
+                        size = (enSize)Number2; 
+                    }
+                    
+                    Result += Generate();
+                }
+
+                lbResult.Text = Result;
+
+                return;
+
+            }
+
+
+            for(int i = 1; i<= numericUpDown1.Value;i++)
+            {
+                Result += Generate(); 
+            }
+
+            lbResult.Text = Result;
+        }
+
+        private void CapitalLetter_Click(object sender, EventArgs e)
+        {
+            size = enSize.CapitalLetter;
+        }
+
+        private void SmallLetter_Click(object sender, EventArgs e)
+        {
+            size = enSize.SmallLetter;
+        }
+
+        private void rbLetter_CheckedChanged(object sender, EventArgs e)
+        {
+            status = enStatus.Letters;
+        }
+
+        private void rbNumbers_CheckedChanged(object sender, EventArgs e)
+        {
+            status = enStatus.Numbers;
+        }
+
+        private void rbSpecialCharacter_CheckedChanged(object sender, EventArgs e)
+        {
+            status = enStatus.SpecialCharacter;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            status = enStatus.Mix;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            status = enStatus.Letters;
+        }
+    }
+}
